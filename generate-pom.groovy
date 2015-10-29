809104aeb1e1cee1d488a8c831247426e6b2ec19
Nick Balkissoon <nbalkiss@Nicks-MacBook-Pro.local>
2015-10-29 15:40:25 -0400

    splitting up scripts

--- generate-pom.groovy
+++ generate-pom.groovy
@@ -2,28 +2,27 @@ import groovy.xml.MarkupBuilder
 import groovy.json.JsonSlurper
 
 def slurper=new JsonSlurper()
-def projects
-projects=slurper.parseText('{"jobs":{"struts":{"dependencies":["1","2"]},"struts2":{"dependencies":["3","4"]}}}')
-
-
+def projects=slurper.parseText('{"jobs":{"struts":{"dependencies":["1","2"]},"struts2":{"dependencies":["3","4"]}}}')
 
 projects.jobs.each {component, val ->
     def writer = new FileWriter(new File("${component}-pom.xml"))
     def xml = new MarkupBuilder(writer)
     xml.project{
-    modelVersion()
-    groupId(component)
-    artifactId()
-    version()
-    name()
-    dependencies{
-        val.dependencies.each {  dep-> 
-            dependency{
-                groupId(dep)
-                artifactId()
-                version()
+        modelVersion()
+        groupId(component)
+        artifactId()
+        version()
+        name()
+        dependencies{
+            val.dependencies.each {  dep-> 
+                dependency{
+                    groupId(dep)
+                    artifactId()
+                    version()
+                }
             }
         }
     }
+userContent("${component}-pom.xml",streamFileFromWorkspace("${component}-pom.xml"))
 }
-}
+
