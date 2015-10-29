bb9869a9be2574707541e80d2ca21f0cbfcaef1d
Nick Balkissoon <nbalkiss@Nicks-MacBook-Pro.local>
2015-10-29 16:41:06 -0400

    updated xml generation and created new template for JSON projects

--- create-jobs.groovy
+++ create-jobs.groovy
@@ -5,20 +5,18 @@ def projects=slurper.parseText(readFileFromWorkspace('jobs.json'))
 
 projects.jobs.each {component, val ->
     job("${component}") {
-	    scm {
-	        git('git://github.com/ngbalk/test-sonatype.git')
-	    }
 	    triggers {
 	        scm('*/15 * * * *')
 	    }
 	    steps {
+	    	shell("wget -O pom.xml localhost:8080/jenkins/userContent/${component}-pom.xml")
 	        maven{
 	          goals('clean install')
 	          mavenInstallation('Maven 3.3.3')
 	        }
 	        shell("curl -u admin:admin123 -X POST -H 'Content-Type: application/json' -d '{\"publicId\":\"${component}\",\"name\": \"${component}\",\"organizationId\":\"e85ccd6ec0664bb4b5a5b490fe0829f6\"}' 'localhost:8070/api/v2/applications'")
 	    }
-	    
+	    userContent("${component}-pom.xml",streamFileFromWorkspace("${component}-pom.xml"))
 	    
 	    configure { project ->
 	        project / publishers << 'com.sonatype.insight.ci.hudson.PostBuildScan'(plugin: 'sonatype-clm-ci@2.14.2-01') {
--- generate-pom.groovy
+++ generate-pom.groovy
@@ -2,27 +2,26 @@ import groovy.xml.MarkupBuilder
 import groovy.json.JsonSlurper
 
 def slurper=new JsonSlurper()
-def projects=slurper.parseText('{"jobs":{"struts":{"dependencies":["1","2"]},"struts2":{"dependencies":["3","4"]}}}')
+def projects=slurper.parseText(new File("jobs.json").text)
 
 projects.jobs.each {component, val ->
     def writer = new FileWriter(new File("${component}-pom.xml"))
     def xml = new MarkupBuilder(writer)
     xml.project{
-        modelVersion()
-        groupId(component)
-        artifactId()
-        version()
-        name()
+        modelVersion(val.modelVersion)
+        groupId(val.groupId)
+        artifactId(val.artifactId)
+        version(val.version)
+        name(val.name)
         dependencies{
-            val.dependencies.each {  dep-> 
+            val.dependencies.each {dep-> 
                 dependency{
-                    groupId(dep)
-                    artifactId()
-                    version()
+                    groupId(dep.groupId)
+                    artifactId(dep.artifactId)
+                    version(dep.version)
                 }
             }
         }
     }
-userContent("${component}-pom.xml",streamFileFromWorkspace("${component}-pom.xml"))
 }
 
