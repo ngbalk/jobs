45115ff4d75ccc16c890a2c64781fef17e66ad04
Nick Balkissoon <nbalkiss@Nicks-MacBook-Pro.local>
2015-11-02 11:06:32 -0500

    parameterized organizationId in REST call to Nexus REST API

--- create-jobs.groovy
+++ create-jobs.groovy
@@ -3,6 +3,9 @@ import groovy.xml.MarkupBuilder
 def slurper=new JsonSlurper()
 def projects=slurper.parseText(readFileFromWorkspace('jobs.json'))
 
+//Manually create organization and copy organizationId here
+def organizationId="9e8ee77412304ec596d3588d9cb4f20b"
+
 projects.jobs.each {component, val ->
     job("${component}") {
 	    triggers {
@@ -14,7 +17,7 @@ projects.jobs.each {component, val ->
 	          goals('clean install')
 	          mavenInstallation('Maven 3.3.3')
 	        }
-	        shell("curl -u admin:admin123 -X POST -H 'Content-Type: application/json' -d '{\"publicId\":\"${component}\",\"name\": \"${component}\",\"organizationId\":\"9e8ee77412304ec596d3588d9cb4f20b\"}' 'localhost:8070/api/v2/applications'")
+	        shell("curl -u admin:admin123 -X POST -H 'Content-Type: application/json' -d '{\"publicId\":\"${component}\",\"name\": \"${component}\",\"organizationId\":\"${organizationId}\"}' 'localhost:8070/api/v2/applications'")
 	    }
 	    userContent("${component}-pom.xml",streamFileFromWorkspace("${component}-pom.xml"))
 	    
