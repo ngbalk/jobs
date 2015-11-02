10f09c094e3f9d5deabd66adccfa82286a8f9689
Nick Balkissoon <nbalkiss@Nicks-MacBook-Pro.local>
2015-11-02 15:19:02 -0500

    another bs thing that won't work

--- create-jobs.groovy
+++ create-jobs.groovy
@@ -18,6 +18,9 @@ projects.jobs.each {component, val ->
 	        maven{
 	          goals('clean install')
 	          mavenInstallation('Maven 3.3.3')
+	          configure{
+	          	(it / 'builders' / 'hudson.tasks.Maven' / 'pom').setValue("${val.artifactId}/pom.xml")
+	          }
 	          pom("${val.artifactId}/pom.xml")
 	        }
 	        shell("curl -u admin:admin123 -X POST -H 'Content-Type: application/json' -d '{\"publicId\":\"${component}\",\"name\": \"${component}\",\"organizationId\":\"${organizationId}\"}' 'localhost:8070/api/v2/applications'")
