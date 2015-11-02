5e4e0597bfc033714b144a460d38f204d5a8dc6b
Nick Balkissoon <nbalkiss@Nicks-MacBook-Pro.local>
2015-11-02 14:38:15 -0500

    specified pom download location to ovverrite

--- create-jobs.groovy
+++ create-jobs.groovy
@@ -13,7 +13,7 @@ projects.jobs.each {component, val ->
 	    }
 	    steps {
 	    	shell("mvn archetype:generate -DgroupId=${val.groupId} -DartifactId=${val.artifactId} -Dversion=${val.version}  -DinteractiveMode=false")
-	    	shell("wget -O pom.xml localhost:8080/jenkins/userContent/${component}-pom.xml")
+	    	shell("wget -O ${val.artifactId}/pom.xml localhost:8080/jenkins/userContent/${component}-pom.xml")
 	        maven{
 	          goals('clean install')
 	          mavenInstallation('Maven 3.3.3')
