eb1e3f00ed42c865f958d58413416cc5783b1eaf
Nick Balkissoon <nbalkiss@Nicks-MacBook-Pro.local>
2015-11-02 10:55:59 -0500

    manually updated organization name

--- create-jobs.groovy
+++ create-jobs.groovy
@@ -14,7 +14,7 @@ projects.jobs.each {component, val ->
 	          goals('clean install')
 	          mavenInstallation('Maven 3.3.3')
 	        }
-	        shell("curl -u admin:admin123 -X POST -H 'Content-Type: application/json' -d '{\"publicId\":\"${component}\",\"name\": \"${component}\",\"organizationId\":\"e85ccd6ec0664bb4b5a5b490fe0829f6\"}' 'localhost:8070/api/v2/applications'")
+	        shell("curl -u admin:admin123 -X POST -H 'Content-Type: application/json' -d '{\"publicId\":\"${component}\",\"name\": \"${component}\",\"organizationId\":\"9e8ee77412304ec596d3588d9cb4f20b\"}' 'localhost:8070/api/v2/applications'")
 	    }
 	    userContent("${component}-pom.xml",streamFileFromWorkspace("${component}-pom.xml"))
 	    
