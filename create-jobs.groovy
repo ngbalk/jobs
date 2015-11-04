01e7763443c80111ec307c068f53fdbf18ca715b
Nick Balkissoon <nbalkiss@Nicks-MacBook-Pro.local>
2015-11-04 11:48:54 -0500

    paramterized username and password and deleted extra code

--- create-jobs.groovy
+++ create-jobs.groovy
@@ -1,23 +1,21 @@
 import groovy.json.JsonSlurper
-import groovy.xml.MarkupBuilder
+
 def slurper=new JsonSlurper()
 def projects=slurper.parseText(readFileFromWorkspace('jobs.json'))
 
-//Manually create organization and copy organizationId here
 def organizationId="9e8ee77412304ec596d3588d9cb4f20b"
+def nexusUsername="admin"
+def nexusPassword="admin123"
 
 projects.jobs.each {component, val ->
     job("${component}") {
-	    triggers {
-	        scm('*/15 * * * *')
-	    }
 	    steps {
 	    	shell("wget -O pom.xml localhost:8080/jenkins/userContent/${component}-pom.xml")
 	        maven{
 	          goals('clean install')
 	          mavenInstallation('Maven 3.3.3')
 	        }
-	        shell("curl -u admin:admin123 -X POST -H 'Content-Type: application/json' -d '{\"publicId\":\"${component}\",\"name\": \"${component}\",\"organizationId\":\"${organizationId}\"}' 'localhost:8070/api/v2/applications'")
+	        shell("curl -u ${nexusUsername}:${nexusPassword} -X POST -H 'Content-Type: application/json' -d '{\"publicId\":\"${component}\",\"name\": \"${component}\",\"organizationId\":\"${organizationId}\"}' 'localhost:8070/api/v2/applications'")
 	    }
 	    userContent("${component}-pom.xml",streamFileFromWorkspace("${component}-pom.xml"))
 	    
