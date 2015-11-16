8cb492a4160cfb77489a58a78424405913593dc7
Nick Balkissoon <nbalkiss@Nicks-MacBook-Pro.local>
2015-11-16 13:21:30 -0500

    propertized, now testing

--- create-jobs.groovy
+++ create-jobs.groovy
@@ -1,13 +1,26 @@
 import groovy.json.JsonSlurper
 
+Properties properties = new Properties()
+File propertiesFile = new File('config.properties')
+propertiesFile.withInputStream {
+    properties.load(it)
+}
+
+
 def slurper=new JsonSlurper()
 def projects=slurper.parseText(readFileFromWorkspace('jobs.json'))
 
-def organizationId="28dba0d94c7e452d9dd1a32e2a51f5f6"
-def nexusUsername="admin"
-def nexusPassword="admin123"
-def blackduckUsername="sysadmin"
-def blackduckPassword="blackduck"
+//def organizationId="28dba0d94c7e452d9dd1a32e2a51f5f6"
+//def nexusUsername="admin"
+//def nexusPassword="admin123"
+
+//def blackduckUsername="sysadmin"
+//def blackduckPassword="blackduck"
+//def blackduckAddress="10.3.12.8"
+
+def blackduckUsername=properties["blackduckUsername"]
+def blackduckPassword=properties["blackduckPassword"]
+def blackduckAddress=properties["blackduckAddress"]
 
 projects.jobs.each {component, val ->
     job("${component}") {
@@ -17,29 +30,29 @@ projects.jobs.each {component, val ->
 	          goals('clean install')
 	          mavenInstallation('Maven 3.3.3')
 	        }
-	        shell("curl -u ${nexusUsername}:${nexusPassword} -X POST -H 'Content-Type: application/json' -d '{\"publicId\":\"${component}\",\"name\": \"${component}\",\"organizationId\":\"${organizationId}\"}' 'localhost:8070/api/v2/applications'")
+//	        shell("curl -u ${nexusUsername}:${nexusPassword} -X POST -H 'Content-Type: application/json' -d '{\"publicId\":\"${component}\",\"name\": \"${component}\",\"organizationId\":\"${organizationId}\"}' 'localhost:8070/api/v2/applications'")
 	        
 //	        shell("curl -X POST --data 'j_username=sysadmin&j_password=blackduck' -i http://10.3.12.8:8080/j_spring_security_check")
 //	        shell("curl -X POST --header 'Content-Type: application/vnd.blackducksoftware.project-1+json' --header 'Accept: application/json' -d '{description': '','name': '${component}','projectTier': 1,'source': 'CUSTOM'}' 'http://10.3.12.8:8080/api/projects'")
 
-	        shell("wget -O scan.cli.zip http://10.3.12.8:8080/download/scan.cli.zip")
+	        shell("wget -O scan.cli.zip http://${blackduckAddress}:8080/download/scan.cli.zip")
 	        shell("unzip -o scan.cli.zip")
-	        shell("bash scan.cli-*/bin/scan.cli.sh --username ${blackduckUsername} --password ${blackduckPassword} --host 10.3.12.8 --port 8080 target")
+	        shell("bash scan.cli-*/bin/scan.cli.sh --username ${blackduckUsername} --password ${blackduckPassword} --host ${blackduckAddress} --port 8080 target")
 	    }
 	    userContent("${component}-pom.xml",streamFileFromWorkspace("${component}-pom.xml"))
 	    
-	    configure { project ->
-	        project / publishers << 'com.sonatype.insight.ci.hudson.PostBuildScan'(plugin: 'sonatype-clm-ci@2.14.2-01') {
-	          applicationSelectType {
-	            value('list')
-	            applicationId("${component}")
-	          }
-	          pathConfig()
-	          failOnSecurityAlerts false
-	          failOnClmServerFailures false
-	          stageId('build')
-	        }
-	    }
+//	    configure { project ->
+//	        project / publishers << 'com.sonatype.insight.ci.hudson.PostBuildScan'(plugin: 'sonatype-clm-ci@2.14.2-01') {
+//	          applicationSelectType {
+//	            value('list')
+//	            applicationId("${component}")
+//	          }
+//	          pathConfig()
+//	          failOnSecurityAlerts false
+//	          failOnClmServerFailures false
+//	          stageId('build')
+//	        }
+//	    }
 	}
 	queue("${component}")
 }
