dfdd07a749d068931643869ecb99ce53a0158241
Nick Balkissoon <nbalkiss@Nicks-MacBook-Pro.local>
2015-12-07 11:38:04 -0500

    uncommented comments

--- jpmc-email-parser.py
+++ jpmc-email-parser.py
@@ -10,54 +10,54 @@ import json
 ################################################################
 
 
-# detach_dir = '.'
-# if 'attachments' not in os.listdir(detach_dir):
-#     os.mkdir('attachments')
+detach_dir = '.'
+if 'attachments' not in os.listdir(detach_dir):
+    os.mkdir('attachments')
 
-# userName = "redhat.jpmc.pilot"
-# passwd = "redhat123"
+userName = "redhat.jpmc.pilot"
+passwd = "redhat123"
 
-# try:
-#     imapSession = imaplib.IMAP4_SSL('imap.gmail.com')
-#     typ, accountDetails = imapSession.login(userName, passwd)
-#     if typ != 'OK':
-#         print 'Not able to sign in!'
-#         raise
+try:
+    imapSession = imaplib.IMAP4_SSL('imap.gmail.com')
+    typ, accountDetails = imapSession.login(userName, passwd)
+    if typ != 'OK':
+        print 'Not able to sign in!'
+        raise
     
-#     imapSession.select('[Gmail]/All Mail')
-#     typ, data = imapSession.search(None, '(UNSEEN)')
-#     if typ != 'OK':
-#         print 'Error searching Inbox.'
-#         raise
+    imapSession.select('[Gmail]/All Mail')
+    typ, data = imapSession.search(None, '(UNSEEN)')
+    if typ != 'OK':
+        print 'Error searching Inbox.'
+        raise
     
-#     # Iterating over all emails
-#     for msgId in data[0].split():
-#         typ, messageParts = imapSession.fetch(msgId, '(RFC822)')
-#         if typ != 'OK':
-#             print 'Error fetching mail.'
-#             raise
+    # Iterating over all emails
+    for msgId in data[0].split():
+        typ, messageParts = imapSession.fetch(msgId, '(RFC822)')
+        if typ != 'OK':
+            print 'Error fetching mail.'
+            raise
 
-#         emailBody = messageParts[0][1]
-#         mail = email.message_from_string(emailBody)
-#         for part in mail.walk():
-#             if part.get_content_maintype() == 'multipart':
-#                 # print part.as_string()
-#                 continue
-#             if part.get('Content-Disposition') is None:
-#                 # print part.as_string()
-#                 continue
-#             fileName = part.get_filename()
+        emailBody = messageParts[0][1]
+        mail = email.message_from_string(emailBody)
+        for part in mail.walk():
+            if part.get_content_maintype() == 'multipart':
+                # print part.as_string()
+                continue
+            if part.get('Content-Disposition') is None:
+                # print part.as_string()
+                continue
+            fileName = part.get_filename()
 
-#             if bool(fileName):
-#                 filePath = os.path.join(detach_dir, 'attachments', fileName)
-#                 print fileName
-#                 fp = open(filePath, 'wb')
-#                 fp.write(part.get_payload(decode=True))
-#                 fp.close()
-#     imapSession.close()
-#     imapSession.logout()
-# except :
-#     print 'Not able to download all attachments.'
+            if bool(fileName):
+                filePath = os.path.join(detach_dir, 'attachments', fileName)
+                print fileName
+                fp = open(filePath, 'wb')
+                fp.write(part.get_payload(decode=True))
+                fp.close()
+    imapSession.close()
+    imapSession.logout()
+except :
+    print 'Not able to download all attachments.'
 
 ################################################################################
 ########### Parse Attachments and Create Golden components.json File ###########
@@ -100,9 +100,9 @@ outFile.write(jsonData)
 ##### Clear Downloaded Attachments #######
 ##########################################
 
-# filelist = [f for f in os.listdir("attachments")]
-# for f in filelist:
-#     os.remove("attachments/"+f)
-# os.rmdir("attachments")
+filelist = [f for f in os.listdir("attachments")]
+for f in filelist:
+    os.remove("attachments/"+f)
+os.rmdir("attachments")
 
 
